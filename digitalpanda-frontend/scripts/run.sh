cb2b693f62c4d61c65eeab78bff367f5a8b0c829
JoelVallone <joel.vallone@gmail.com>
2022-02-22 17:23:18 +0100

    Cleanup scripts

--- digitalpanda-frontend/scripts/run.sh
+++ digitalpanda-frontend/scripts/run.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
+#
 # Run the full digitalpanda stack on the local dev machine
 #
 # Prerequisite:
-# - Ansible role "digitalpanda-base" already initialized the node config
-#     See lab's main README.md
-# - Docker is installed
-# - Docker can be run without sudo by the user running this script
-#     See: https://docs.docker.com/engine/install/linux-postinstall/
-# - Python 2.7 dependencies to run the ansible-docker module (for panda-config user)
-#     > pip2 uninstall urllib3
-#     > pip2 install docker requests
-#     > rm -rf ~/.local/lib/python2.7/site-packages/requests/packages/urllib3/
+#   - Ansible role "digitalpanda-base" already initialized the node config
+#       See lab's main README.md
+#   - Docker is installed on the local dev machine
+#   - Docker can be run without sudo by the user running this script
+#       See: https://docs.docker.com/engine/install/linux-postinstall/
+#   - Python 2.7 dependencies to run the ansible-docker module (for panda-config user)
+#       > pip2 uninstall urllib3
+#       > pip2 install docker requests
+#       > rm -rf ~/.local/lib/python2.7/site-packages/requests/packages/urllib3/
 
 set -e
 
@@ -31,7 +32,7 @@ if [ $# -gt 0 ] && [ $1 = "-b" ];then
     echo "Deploy backend stack with Ansible"
     ansible-playbook digitalpanda-stack.yml \
       --inventory-file=digitalpanda-inventory-local \
-      --extra-vars "inject_test_data=true deploy_frontend=false"
+      --extra-vars "clear_state=true inject_test_data=true deploy_frontend=false"
     docker ps
     cd -
 fi
@@ -41,4 +42,5 @@ echo "Contacting backend REST API"
 curl -v http://localhost:8081/ui/greeting
 
 echo "Build and serve frontend"
-ng serve --disableHostCheck --host=0.0.0.0
+cd "${SCRIPT_FOLDER}/../"
+npm run start
